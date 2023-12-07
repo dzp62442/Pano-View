5dd2bebb6b27e226aa031d3bad4ad49975b88807
dzp62442 <dzp62442@163.com>
2023-12-07 17:40:42 +0800

    新版本作为一个单独分支

--- src/main.cpp
+++ src/main.cpp
@@ -7,294 +7,58 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <exception>
-#include <GL/glut.h>
+#include <GLFW/glfw3.h>
+#include <GL/glew.h> // 使用GLEW管理OpenGL函数
+#include <GL/gl.h>   // 核心OpenGL功能
+#include <glm/glm.hpp> // 用于数学运算，例如向量和矩阵
+#include <glm/gtc/matrix_transform.hpp> // 提供矩阵变换功能
 #include <string>
-
-//! 全局变量
-time_t start;
-time_t eend;
-GLfloat  xangle = 0.0;    //X 旋转量
-GLfloat  yangle = 0.0;    //Y 旋转量
-GLfloat  zangle = 0.0;    //Z 旋转量
-float vertex[61][31][3];
-float texpoint[61][31][2];
-cv::VideoCapture cap;
-int Width = 0;
-int Height = 0;
-static GLuint texName[4];//存放纹理
-static GLint Model = 0; // 模型 calllist 号
-static double Ox = 0;   // 视角旋转参数
-static double Oy = 0;
-static double direction = 0; //站立位置及前进方向；
-static double step = 0.5;
-GLUquadricObj*g_text;//曲面，制作一个包围房间的大球体做背景
-//交叉点的坐标
-int cx = 0;
-int cy = 0;
-
-//初始化
-void init(void)
-{
-    glClearColor (0.0, 0.0, 0.0, 0.0);    //清理颜色，为黑色，（也可认为是背景颜色）
-
-    glCullFace(GL_FRONT);    //背面裁剪(背面不可见)
-    glEnable(GL_CULL_FACE);    //启用裁剪
-    glEnable(GL_TEXTURE_2D);
-    
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);//所读取的图像数据的行对齐方式
-
-    //初始化数据
-    for(int i=0; i<=360; i+=6)
-    {
-        for( int j=180; j>=0; j-=6)
-        {
-            vertex[i/6][(180-j)/6][0] = cos((float)i/180.0*CV_PI)*sin((float)j/180.0*CV_PI)*2;
-            vertex[i/6][(180-j)/6][1] = cos((float)j/180.0*CV_PI)*2;
-            vertex[i/6][(180-j)/6][2] = sin((float)i/180.0*CV_PI)*sin((float)j/180.0*CV_PI)*2;
-        }
-    }
-    for(int i=0; i<61; i++)
-    {
-        for(int k=0; k<31; k++)
-        {
-            texpoint[i][k][0]= (float)i/60.0;        //生成X浮点值
-            texpoint[i][k][1]= (float)k/30.0;        //生成Y浮点值
-        }
-    }
-    printf("...\n");
-    glGenTextures(1, &texName[0]);//用来生成纹理的数量  存储纹理索引的第一个元素指针
-    glBindTexture(GL_TEXTURE_2D, texName[0]);//绑定（指定）纹理对象
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);   //纹理参数：坐标超过0.0~1.0f时的处理方式--重复
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  //纹理参数：贴图插值方式--最近邻 / 线性插值 GL_LINEAR
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-}
-
-void loadTexture(const std::string &filename, GLuint &texture)
-{
-    cv::Mat frame;
-    frame = cv::imread(filename);
-    int ImageWidth = frame.cols;
-    int ImageHeight = frame.rows;
-    flip(frame, frame, 0);
-
-    //加载纹理
-    glGenTextures(1, &texture);//用来生成纹理的数量  存储纹理索引的第一个元素指针
-    glBindTexture(GL_TEXTURE_2D, texture);//绑定（指定）纹理对象
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);   //纹理参数：坐标超过0.0~1.0f时的处理方式--重复
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  //纹理参数：贴图插值方式--最近邻 / 线性插值 GL_LINEAR
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, weight, height, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, frame.data);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,  ImageWidth, ImageHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, frame.data);
-
-    //生成一个2D纹理（Texture）。bmp图片的像素顺序是BGR所以用GL_BGR_EXT来反向加载数据
-}
-
-//从这里开始进行所有的绘制
-void drawCube(void)
-{
-    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);    //清除屏幕和深度缓存
-    glMatrixMode(GL_MODELVIEW);
-    //glLoadIdentity();    //重置当前的模型观察矩阵
-
-    glPushMatrix();
-    {
-        gluLookAt(0, 0, -5, 0, 0, 0, 0, 1, 0);
-        glTranslatef(0.0f, 0.0f, -5.0f);    //移入屏幕 5 个单位
-        glRotatef(xangle, 1.0f, 0.0f, 0.0f);    //绕X轴旋转
-       glRotatef(yangle, 0.0f, 1.0f, 0.0f);    //绕Y轴旋转
-        glRotatef(zangle, 0.0f, 0.0f, 1.0f);    //绕Z轴旋转
-        
-        glBindTexture(GL_TEXTURE_2D, texName[0]);                // 选择纹理,有多个纹理时这句话是必要de
-        glBegin(GL_QUADS); {    //四边形绘制开始
-            for(int i=0; i<60; i++) {
-                for(int j=0; j<30; j++) {
-                    //第一个纹理坐标(左下角)
-                    glTexCoord2f(texpoint[i][j][0], texpoint[i][j][1]);
-                    glVertex3f(vertex[i][j][0], vertex[i][j][1], vertex[i][j][2]);
-
-                    //第二个纹理坐标(左上角)
-                    glTexCoord2f(texpoint[i][j][0], texpoint[i][j+1][1]);
-                    glVertex3f(vertex[i][j+1][0], vertex[i][j+1][1], vertex[i][j+1][2]);
-
-                    //第三个纹理坐标(右上角)
-                    glTexCoord2f(texpoint[i+1][j+1][0], texpoint[i+1][j+1][1]);
-                    glVertex3f(vertex[i+1][j+1][0], vertex[i+1][j+1][1], vertex[i+1][j+1][2]);
-
-                    //第四个纹理坐标(右下角)
-                    glTexCoord2f(texpoint[i+1][j][0], texpoint[i][j][1]);
-                    glVertex3f(vertex[i+1][j][0], vertex[i+1][j][1], vertex[i+1][j][2]);
-                }
-            }
-        }glEnd();
-
-    }
-    glPopMatrix();
-    glFlush();
-}
-
-
-void display(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);    //清楚颜色数据和深度数据（清屏）
-    //glLoadIdentity();    //Reset The View
-    glPushMatrix(); // 保存当前模型视图矩阵
-    glRotatef((GLfloat)direction, 0.0, 0.0, 1.0);   // 用变换后(旋转或移动)模型坐标系建立模型
-    glTranslated(Ox, Oy, 0.0);
-
-    // looploadTexture(texName[0]);
-    loadTexture("../resources/total.bmp", texName[0]);
-    drawCube();
-
-    glutSwapBuffers();    //交换缓冲区。显示图形
-    
-}
-
-//当窗口大小改变时，会调用这个函数
-void reshape(GLsizei w,GLsizei h)
-{
-    glViewport(0,0,w,h);    //设置视口
-    glMatrixMode(GL_PROJECTION);    //设置矩阵模式为投影变换矩阵，
-    glLoadIdentity();    //变为单位矩阵
-    gluPerspective(110, (GLfloat)w / h, 0.1f, 100.0f);    //设置投影矩阵
-    glMatrixMode(GL_MODELVIEW);    //设置矩阵模式为视图矩阵(模型)
-    glLoadIdentity();    //变为单位矩阵
-}
-
-//处理鼠标点击
-void Mouse(int button, int state, int x, int y)
-{
-    if(state == GLUT_DOWN) //第一次鼠标按下时,记录鼠标在窗口中的初始坐标
-    {
-        //记住鼠标点击后光标坐标
-        cx = x;
-        cy = y;
-        //printf("Mouse: x=%d, y=%d, oldx_Translatef=%f, oldy_Translatef=%f\n", x, y, oldx_Translatef, oldy_Translatef);
-    }
-}
-
-//处理鼠标拖动
-void onMouseMove(int x, int y)
-{
-    float offset = 0.18;
-    //计算拖动后的偏移量，然后进行xy叠加减
-    yangle -= ((x - cx) * offset);
-
-    if (xangle < 90 && y > cy) {//往下拉
-        xangle += ((y - cy) * offset);
-    } else if (xangle > -90 && y < cy) {//往上拉
-        xangle += ((y - cy) * offset);
-    }
-    glutPostRedisplay();
-
-    //保存好当前拖放后光标坐标点
-    cx = x;
-    cy = y;
-}
-
-//键盘输入事件函数
-void keyboard(unsigned char key,int x,int y)
-{
-    switch(key)
-    {
-        case 'x':        //当按下键盘上d时，以沿X轴旋转为主
-            if (xangle < 85.0f)
-            {
-                xangle += 1.0f;    //设置旋转增量
-            }
-            break;
-        case 'X':
-            if (xangle > -85.0f)
-            {
-                xangle -= 1.0f;    //设置旋转增量
-            }
-            break;
-        case 'y':
-            yangle += 1.0f;
-            break;
-        case 'Y':
-            yangle -= 1.0f;
-            break;
-        case 'w':
-        case 'W':
-            Ox -= step*sin(direction / 180 * CV_PI);
-            Oy -= step*cos(direction / 180 * CV_PI);
-            glutPostRedisplay();
-            break;
-        case 's':
-        case 'S':
-            Ox += step*sin(direction / 180 *CV_PI);
-            Oy += step*cos(direction / 180 *CV_PI);
-            glutPostRedisplay();
-            break;
-        case 'a':
-        case 'A':
-            direction = direction - 2;
-            glutPostRedisplay();
-            break;
-        case 'd':
-        case 'D':
-            direction = direction + 2;
-            glutPostRedisplay();
-            break;
-        default:
-            return;
-    }
-    glutPostRedisplay();    //重绘函数
-}
-
-//特殊按键
-void specialKey(int key, int x, int y)
-{
-    float offset = 1.5;
-    switch (key)
-    {
-    case GLUT_KEY_UP:    //脑袋向上往前看
-        if (xangle < 90.0f)
-        {
-            xangle += offset;    //设置旋转增量
-        }
-        break;
-    case GLUT_KEY_DOWN:    //脑袋向下往前看
-        if (xangle > -90.0f)
-        {
-            xangle -= offset;    //设置旋转增量
-        }
-        break;
-    case GLUT_KEY_LEFT:    //脑袋想左往前看
-        yangle -= offset;
-        break;
-    case GLUT_KEY_RIGHT:    //脑袋向右往前看
-        yangle += offset;
-        break;
-    default:
-        break;
-    }
-    glutPostRedisplay();
-}
+#include "mesh.hpp"
+#include "camera.hpp"
 
 int main(int argc, char *argv[])
 {
-    printf("可通过↑↓←→按键控制全景图绕旋转\n");
-    glutInit(&argc, argv);    //固定格式
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowSize(800, 800);    //显示框的大小
-    glutInitWindowPosition(100,100);    //确定显示框左上角的位置
-    glutCreateWindow("OpenGLsurroundview");
+    // 初始化GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Panorama Viewer", NULL, NULL);
+    glfwMakeContextCurrent(window);
 
-    init();    //初始化资源,这里一定要在创建窗口以后，不然会无效
+    // 初始化OpenGL状态
+    // 设置视口，配置投影等
 
-    glutDisplayFunc(display);
-    // glutTimerFunc(40, timeFunc, 0);
+    // 加载全景图像
+    GLuint panoramaTexture = loadPanoramaTexture("path/to/panorama.jpg");
 
-    glutReshapeFunc(reshape); 
-    glutMouseFunc(Mouse);
-    glutMotionFunc(onMouseMove);
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(specialKey); // 特殊按键
+    // 创建球面网格
+    Mesh sphereMesh = createSphereMesh();
 
-    glutMainLoop();
+    // 设置虚拟相机
+    Camera camera;
+    camera.setPosition(0, 0, 0); // 球面中心
+    camera.setPerspective(fov, aspect, near, far);
+
+    // 渲染循环
+    while(!glfwWindowShouldClose(window)) {
+        // 处理用户输入
+        processInput(window);
+
+        // 更新相机视角等
+
+        // 渲染
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        renderSphere(panoramaTexture, sphereMesh, camera);
+
+        // 交换缓冲区并轮询IO事件
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // 清理资源
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
 
     return 0;
 }
