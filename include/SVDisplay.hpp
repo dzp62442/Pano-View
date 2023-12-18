86537cbc52501a38e172d99f2a6dc035644e07eb
dzp62442 <dzp62442@163.com>
2023-12-18 15:35:31 +0800

    调整相机视点

--- include/SVDisplay.hpp
+++ include/SVDisplay.hpp
@@ -13,7 +13,7 @@ using int32 = int32_t;
 // 第一项控制视点位置，第二项控制浏览时的视野旋转轴
 // 第一项中y表示视点高度，设为1.7-1.9，调节x和z来控制视点在车身周围的位置
 // x控制视点在车身前后，z控制视点在车身左右
-static Camera cam(glm::vec3(0.5, 1.9, 0.), glm::vec3(0.0, 1.0, 0.0));  // 观察视点
+static Camera cam(glm::vec3(0., 0., 0.), glm::vec3(0.0, 1.0, 0.0));  // 观察视点
 
 class SVDisplayView
 {
