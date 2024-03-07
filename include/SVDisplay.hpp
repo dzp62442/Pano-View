e0f1c203226fbd644b7ae0ce4d5ab14ec1f73f37
dzp62442 <dzp62442@163.com>
2024-03-07 21:17:00 +0800

    对比实验

--- include/SVDisplay.hpp
+++ include/SVDisplay.hpp
@@ -19,7 +19,7 @@ using int32 = int32_t;
 // 第一项中y表示视点高度，设为1.7-1.9，调节x和z来控制视点在车身周围的位置
 // x控制视点在车身前后，z控制视点在车身左右
 // static Camera cam(glm::vec3(0., 0., 0.), glm::vec3(0.0, 1.0, 0.0));  // 球模型观察视点
-static Camera cam(glm::vec3(0.5, 1.9, 0.), glm::vec3(0.0, 1.0, 0.0));  // 碗模型观察视点
+static Camera cam(glm::vec3(0., 1.9, 0.), glm::vec3(0.0, 1.0, 0.0));  // 碗模型观察视点
 
 class SVDisplayView
 {
